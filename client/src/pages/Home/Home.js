59059144d43788734c980cc66e45fe18fdc285dc
hnslyswhy <huanyuw01@gmail.com>
2021-12-24 05:23:44 -0800

    scss starter code

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ProjectForm from "../../components/ProjectForm/ProjectForm";
 import astronaut from "../../assets/images/astronaut.jpeg";
+import "./Home.scss";
 
 const Home = () => {
   const [isStarted, setIsStarted] = useState(false);
@@ -8,11 +9,14 @@ const Home = () => {
     setIsStarted(!isStarted);
   };
   return (
-    <main>
+    <main className="main">
       {!isStarted && (
-        <div>
-          <img src={astronaut} alt="astronaut" />
-          <h1 onClick={handleClick}>Start</h1>
+        <div className="home__container">
+          <h1 className="home__title">Project Count Down</h1>
+          <img className="home__image" src={astronaut} alt="astronaut" />
+          <h2 className="home__start" onClick={handleClick}>
+            Start
+          </h2>
         </div>
       )}
       <ProjectForm /> {/* fpr style purpose only */}
